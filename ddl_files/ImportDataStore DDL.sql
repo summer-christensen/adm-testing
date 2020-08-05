78a4b83bfbfc2539717d12bcd24aaa1110a62825
Summer Christensen <summer.christensen@appian.com>
2020-08-05 15:03:21 -0700

    Committed by summer-christensen

---
+++ ddl_files/ImportDataStore DDL.sql
@@ -0,0 +1,19 @@
+/*
+Data Store: ImportDataStore
+Created: 2020-08-05T21:50:57.892Z
+By: admin.user
+Appian Version: 20.4.0.75
+Target Database: MariaDB 10.5.4-MariaDB
+Database Driver: MariaDB Connector/J 2.6.0
+*/
+
+/* UPDATE DDL */
+/* DROP AND CREATE DDL */
+/* WARNING: The DDL commented out below will drop and re-create all tables.
+    drop table if exists `importdatatype`;
+
+    create table `importdatatype` (
+        `id` integer not null,
+        primary key (`id`)
+    ) ENGINE=InnoDB;
+*/
